f14cdcf4ec63d6ddea8657b1206690f97333e753
Alan Lee <alan.lee@procore.com>
2025-03-15 10:12:27 -0700

    clean: kernel params

--- hosts/shimmer/boot.nix
+++ hosts/shimmer/boot.nix
@@ -8,7 +8,7 @@
         canTouchEfiVariables = true;
       };
     };
-    kernelParams = [ "i915.force_probe=7d55" "i915.enable_psr=0" "usbcore.autosuspend=-1" ];
+    kernelParams = [ "intel_pstate=disable" "i915.force_probe=7d55" "i915.enable_psr=0" "usbcore.autosuspend=-1" "threadirqs" ];
     initrd = {
       kernelModules = ["xe"];
     };
