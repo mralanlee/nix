8bee8d101e838aca244ffa1530da98cec8c7c609
Alan Lee <alanleunglee@gmail.com>
2025-04-15 21:42:15 -0700

    refactor: set homebrew to be managed by nix

---
+++ home/alan/darwin/homebrew.nix
@@ -0,0 +1,18 @@
+{
+  homebrew = {
+    casks = [
+      aerospace
+      betterdisplay
+      karabiner-elements
+    ];
+    brews = [
+      tfenv
+    ];
+
+    # mac store apps
+    masApps = {
+      Amphetamine = 937984704;
+      Flycut = 442160987;
+    };
+  }
+}
