6be90800e3744804b3dd82e650e3c71d6ef9e3aa
Alan Lee <alanleunglee@gmail.com>
2025-04-15 22:25:00 -0700

    feat: install baseline packages for every system

--- home/alan/common/packages.nix
+++ home/alan/common/packages.nix
@@ -1,10 +1,35 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    alejandra
-    devbox
+    fd
+    htop
+    jq
+
+    # chat
     telegram-desktop
+    slack
     signal-desktop
     discord
-    nodejs
+
+    # developer tools
+    mise
+    devbox
+    alejandra
+    hub
+    kind
+
+    # containerization and related tooling
+    podman
+    podman-compose
+    podman-tui
+    kubectl
+    k9s
+    lens
+    stern
+
+    # terraform
+    terraform-docs
+    terraform
+    terraform
+    # terraform-ls moved to neovim config
   ];
 }
