06bcaeef0d19ae650a4fa8d3bde0b7f2413d6ef7
Alan Lee <alanleunglee@gmail.com>
2025-04-24 15:13:52 -0700

    feat(yazi): add yazi config

---
+++ home/alan/common/yazi.nix
@@ -0,0 +1,40 @@
+{pkgs, ...} : let
+  yaz-plugins = pkgs.fetchFromGitHub {
+  owner = "yazi-rs";
+		repo = "plugins";
+		rev = "...";
+		hash = "sha256-...";
+  };
+in {
+  programs.yazi = {
+    enable = true;
+    enableZshIntegrations = true;
+    shellWrapperName = "y";
+
+    settings = {
+      manager = {
+        show_hidden = true;
+      };
+      preview = {
+        max_width = 1000;
+        max_height = 1000;
+      }
+    };
+    plugins = {
+			chmod = "${yazi-plugins}/chmod.yazi";
+			full-border = "${yazi-plugins}/full-border.yazi";
+			toggle-pane = "${yazi-plugins}/toggle-pane.yazi";
+			starship = pkgs.fetchFromGitHub {
+				owner = "Rolv-Apneseth";
+				repo = "starship.yazi";
+				rev = "...";
+				sha256 = "sha256-...";
+			};
+		};
+
+		initLua = ''
+			require("full-border"):setup()
+			require("starship"):setup()
+		'';
+  };
+}
