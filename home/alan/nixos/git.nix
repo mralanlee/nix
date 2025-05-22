b68deee605c2292f6558ba8aa53fc9681558b36f
Alan Lee <alanleunglee@gmail.com>
2025-05-21 22:44:54 -0700

    fix: update email

--- home/alan/nixos/git.nix
+++ home/alan/nixos/git.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   programs.git = {
-    userEmail = "alan.lee@procore.com";
+    userEmail = "alanleunglee@gmail.com";
     signing = {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMs8R1vNmoLXco86gdrVF/qkJNOruMw8XplBH4diaUhX alan.lee@procore.com";
     };
