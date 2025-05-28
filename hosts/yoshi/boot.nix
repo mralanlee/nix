036725c649f7808845b21d8c720e48548a9c925a
Alan Lee <alanleunglee@gmail.com>
2025-05-27 17:45:36 -0700

    fix(boot): set to 1

--- hosts/yoshi/boot.nix
+++ hosts/yoshi/boot.nix
@@ -12,5 +12,5 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.consoleMode = "2";
+  boot.loader.systemd-boot.consoleMode = "1";
 }
