1ac4fd40b5d340b889edf1e2565d43e1a6627f00
Alan Lee <alanleunglee@gmail.com>
2025-06-12 13:48:27 -0700

    refactor(ssh): expand ssh config

--- home/alan/common/ssh.nix
+++ home/alan/common/ssh.nix
@@ -1,3 +1,11 @@
 {
-  programs.ssh = {enable = true;};
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+        AddKeysToAgent yes
+        UseKeychain yes
+        IdentityFile ~/.ssh/id_ed25519
+    '';
+  };
 }
