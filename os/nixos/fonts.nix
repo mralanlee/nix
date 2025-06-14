04663bf08b92c9468d68df38c1c12db623541526
Alan Lee <alanleunglee@gmail.com>
2025-06-13 18:42:55 -0700

    fix(1password): 1password on hidpi monitor

--- os/nixos/fonts.nix
+++ os/nixos/fonts.nix
@@ -5,8 +5,20 @@
   ];
 
   fonts.fontconfig = {
+    enable = true;
+    antialias = true;
+    hinting = {
+      enable = true;
+      style = "slight";
+    };
+    subpixel = {
+      rgba = "rgb";
+      lcdfilter = "default";
+    };
     defaultFonts = {
       monospace = ["JetBrains Mono"];
+      sansSerif = ["Inter" "Liberation Sans"];
+      serif = ["Liberation Serif"];
     };
   };
 }
