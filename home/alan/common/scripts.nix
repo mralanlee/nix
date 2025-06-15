a7472d3fe9a305c1c719edf8367bb9bff2d73746
Alan Lee <alanleunglee@gmail.com>
2025-06-14 18:55:56 -0700

    refactor(audio): update audio toggle on waybar

--- home/alan/common/scripts.nix
+++ home/alan/common/scripts.nix
@@ -5,9 +5,15 @@
 }: let
   aws_sso_auth = pkgs.writeShellScriptBin "aws_sso_auth" (builtins.readFile ../assets/scripts/aws_sso_auth);
   aider_copilot = pkgs.writeShellScriptBin "aider_copilot" (builtins.readFile ../assets/scripts/aider_copilot);
+  audio-toggle = pkgs.writeShellScriptBin "audio-toggle" (builtins.readFile ../assets/scripts/audio-toggle);
+  audio-menu-dropdown = pkgs.writeShellScriptBin "audio-menu-dropdown" (builtins.readFile ../assets/scripts/audio-menu-dropdown);
+  mic-menu-dropdown = pkgs.writeShellScriptBin "mic-menu-dropdown" (builtins.readFile ../assets/scripts/mic-menu-dropdown);
 in {
   home.packages = [
     aws_sso_auth
     aider_copilot
+    audio-toggle
+    audio-menu-dropdown
+    mic-menu-dropdown
   ];
 }
