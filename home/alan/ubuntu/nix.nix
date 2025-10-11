d964010471bbc5cc39a940ec91da95b4eac22559
Alan Lee <alanleunglee@gmail.com>
2025-10-11 09:51:16 -0700

    fix: nix

--- home/alan/ubuntu/nix.nix
+++ home/alan/ubuntu/nix.nix
@@ -1,11 +1,11 @@
-{
+{pkgs, ...}: {
   nixpkgs.config = {
     allowUnfree = true;
     allowUnfreePredicate = _: true;
   };
 
   nix = {
-    package = null;  # Use system nix
+    package = pkgs.nix;
     settings = {
       experimental-features = ["nix-command" "flakes"];
     };
