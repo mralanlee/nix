b8c6113dbcaae86f5f0cddc16b9e3c926b6cdc27
Alan Lee <alanleunglee@gmail.com>
2026-01-19 21:22:40 -0800

    fix: nerd fonts

--- home/alan/common/fonts.nix
+++ home/alan/common/fonts.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
-  ];
+  fonts.packages =
+    if pkgs ? nerdfonts then
+      [(pkgs.nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})]
+    else
+      [
+        pkgs."nerd-fonts".fira-code
+        pkgs."nerd-fonts".jetbrains-mono
+      ];
 
   fonts.fontconfig = {
     enable = true;
