56674d01bedc2c612916e73cb2e0d5fe8c17fabb
Alan Lee <alanleunglee@gmail.com>
2026-01-19 21:21:32 -0800

    fix: create override

--- home/alan/common/fonts.nix
+++ home/alan/common/fonts.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.jetbrains-mono
+    (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
   ];
 
   fonts.fontconfig = {
