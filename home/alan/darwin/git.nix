8ddc562338fb95de75b1127a517bb1ef23ce751a
Alan Lee <alanleunglee@gmail.com>
2026-03-20 14:34:28 -0700

    fix(macos): separate git config

--- home/alan/darwin/git.nix
+++ home/alan/darwin/git.nix
@@ -4,6 +4,8 @@
   ...
 }: {
   programs.git.settings = lib.mkIf (hostname == "yoshi-mac") {
+    user.email = lib.mkForce "alanleunglee@gmail.com";
+    user.signingKey = lib.mkForce "";
     commit.gpgsign = lib.mkForce false;
   };
   programs.gh.gitCredentialHelper.enable = true;
